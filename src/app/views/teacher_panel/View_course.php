6c279ff1a0cb4b577ddfe3d06a42193c30ca1981
Jcano <jcano2620k@gmail.com>
2025-11-09 22:25:55 -0500

    reparando

--- src/app/views/teacher_panel/View_course.php
+++ src/app/views/teacher_panel/View_course.php
@@ -8,14 +8,14 @@
   <link rel="stylesheet" href="/public/css/boostrap_dashboard/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
   <link rel="stylesheet" href="/public/css/boostrap_dashboard/style.css">
-  <link rel="stylesheet" href="/public/css/teacher_courses/ver_curso.css">
+  <link rel="stylesheet" href="/public/css/teacher_courses/courses.css">
 
 </head>
 
 <body>
 
-  <?php require_once __DIR__ . "/../../components/teacher/sidebar.php"; ?>
-  <?php require_once __DIR__ . "/../../components/teacher/navbar.php"; ?>
+  <?php require_once __DIR__ . "/../components/teacher/sidebar.php"; ?>
+  <?php require_once __DIR__ . "/../components/teacher/navbar.php"; ?>
 
   <main class="main-content p-4">
 
